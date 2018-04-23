460c97d45b2b7296c27297ee1a5c7f8ac11d1e9a
Drew Cain <drew@simpli.fi>
2018-04-23 17:13:16 -0500

    updated content

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,33 @@
 <template lang="pug">
 .container
-  div
-    h1(class="title") groksrc
-    .links
-      a(href="https://nuxtjs.org/"
-        target="_blank"
-        class="button--green") Documentation
-      a(href="https://github.com/nuxt/nuxt.js"
-        target="_blank"
-        class="button--grey") GitHub
+  .row.justify-content-center
+    .col-6
+      h1(class="title") groksrc search
+  .row.justify-content-center
+    .col-8
+      form
+        .form-group
+          input(type="text" class="form-control" placeholder="Enter a search term")
+  .row.justify-content-center
+    .col-5.center
+      button.btn.btn-primary(type="button") Search
+      button.btn.btn-info.btn-lucky(type="button") I'm feeling lucky
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 70px;
   color: #35495e;
   letter-spacing: 1px;
+  margin-top: 200px;
+  text-align: center;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.btn-lucky {
+  margin-left: 15px;
 }
-
-.links {
-  padding-top: 15px;
+.center {
+  text-align: center;
 }
 </style>
