2071f687e3d4bb7e51408b7310692afba5120380
Drew Cain <drew@simpli.fi>
2018-04-23 16:15:53 -0500

    switch to pug

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,19 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        src
-      </h1>
-      <h2 class="subtitle">
-        groksrc nuxt
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+.container
+  div
+    h1(class="title") groksrc
+    .links
+      a(href="https://nuxtjs.org/"
+        target="_blank"
+        class="button--green") Documentation
+      a(href="https://github.com/nuxt/nuxt.js"
+        target="_blank"
+        class="button--grey") GitHub
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
+
 }
 </script>
 
