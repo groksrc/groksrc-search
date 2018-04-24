bb1b08445938cc770a3b92a1d6336d90a71c457e
Drew Cain <drew@simpli.fi>
2018-04-24 02:15:31 -0500

    better layout for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
 .container
   .row.justify-content-center
-    .col-6
-      h1(class="title") groksrc search
+    .col-8
+      h1.center(class="title") groksrc search
   .row.justify-content-center
     .col-8
       .form-group
         input(type="text" class="form-control" placeholder="Enter a search term" @keyup.enter="search")
   .row.justify-content-center
-    .col-5.center
-      button.btn.btn-primary(type="button" @click="search") Search
-      button.btn.btn-info.btn-lucky(type="button" @click="getLucky") I'm feeling lucky
+    .col-sm-4.center
+      button.btn.btn-primary.w-75(type="button" @click="search") Search
+    .col-sm-4.center
+      button.btn.btn-info.btn-lucky.w-75(type="button" @click="getLucky") I'm feeling lucky
 </template>
 <script>
 export default {
@@ -32,11 +33,8 @@ export default {
   font-size: 70px;
   color: #35495e;
   letter-spacing: 1px;
-  margin-top: 200px;
+  margin-top: 150px;
   text-align: center;
-}
-.btn-lucky {
-  margin-left: 15px;
 }
 .center {
   text-align: center;
